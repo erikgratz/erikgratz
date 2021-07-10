c873e13f954bddf92c90757d8bb810dae2525d91
erikgratz <erik.gratz@sbwell.com>
2021-07-10 08:35:23 -0700

    bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,7 +37,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false
+    wssHost: process.env.MIX_PUSHER_HOST,
+    wssPort: 6001,
+    forceTLS: true,
+    disableStats: true,
+    scheme: process.env.MIX_PUSHER_SCHEME
 });
